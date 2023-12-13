177e676c6971fc4a418a390ba4290d59404c86b0
Sagar Adhikari <sagarjungadhikari19@gmail.com>
2023-12-13 16:39:45 +0545

    counter fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <Counter />
+
     </div>
   );
+}
+
+
+function Counter() {
+
+  const [step, setStep] = useState(1);
+
+  const [Counter, setCounter] = useState(0);
+  return (<>
+
+    <div>
+      <button onClick={() => setStep((s) => s - 1)}
+      >-</button>
+      <span>STEP {step}</span>
+      <button onClick={() => setStep((s) => s + 1)}> + </button>
+
+    </div>
+    <br></br>
+    <div>
+      <button onClick={() => setCounter((c) => c - step)} >-</button>
+      <span>COUNT   {Counter} </span>
+      <button onClick={() => setCounter((c) => c + step)}>+</button>
+
+    </div>
+
+  </>
+
+  )
 }
 
 export default App;
